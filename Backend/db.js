694e1aab0b425abc43bc5536b20d9a2b6eb175f8
Mathew Rafael Bulawan <mathewbulawan@gmail.com>
2025-12-11 10:56:24 +0800

    Deployement of Backend

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,13 @@
-// backend/db.js
 require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.DB_USER,       // 'SDOadmin'
+  host: process.env.DB_HOST,       // 'msss.postgres.database.azure.com'
+  database: process.env.DB_DATABASE, // 'mrsss'
+  password: process.env.DB_PASSWORD, // 'MRSSS'
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false } // required for Azure
 });
 
 module.exports = pool;
