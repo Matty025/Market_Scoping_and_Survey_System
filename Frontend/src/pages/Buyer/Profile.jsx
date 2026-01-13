fca9dbaf5be453c145ba08ad117458faeefbe163
baliwagictu-code <baliwag.ictu@deped.gov.ph>
2026-01-13 10:04:35 +0800

    Debugging supplier's profile

--- Frontend/src/pages/Buyer/Profile.jsx
+++ Frontend/src/pages/Buyer/Profile.jsx
@@ -88,8 +88,3 @@ export default function BuyerProfile() {
     </div>
   );
 }
-import React from "react";
-
-export default function Profile() {
-  return <h2>Buyer Profile Page (Coming Soon)</h2>;
-}
